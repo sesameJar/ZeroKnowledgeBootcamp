496d4f1946701e5dfb6cb911fea090ac093f7711
Mehrad Kavian <mehrad.kavian@gmail.com>
2023-07-28 10:46:50 -0400

    finished functions assignment

--- rust/homeworks/homework4/functions/functions3.rs
+++ rust/homeworks/homework4/functions/functions3.rs
@@ -1,10 +1,9 @@
 // functions3.rs
 // Make me compile! Execute `zustlings hint functions3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_this();
+    let n = 4;
+    call_this(n);
 }
 
 fn call_this(num: u32) {
